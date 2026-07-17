8ad53873daa241ac6567b56be8805d98698203eb
RasiDesh <rasikasdeshpa@gmail.com>
2026-07-17 12:42:35 +0530

    Add SQL queries for operators and constraints
    Added various SQL queries demonstrating relational, boolean, pattern matching, and arithmetic operators.

--- polyclinic.sql
+++ polyclinic.sql
@@ -108,6 +108,9 @@ ALTER TABLE Pharmacy ADD CONSTRAINT UC_med_name UNIQUE(med_name);
 
 -- Check
 ALTER TABLE Pharmacy ADD CONSTRAINT quancheck CHECK (quantity >= 10);
+SELECT CONSTRAINT_NAME, CHECK_CLAUSE
+   FROM INFORMATION_SCHEMA.CHECK_CONSTRAINTS
+   WHERE CONSTRAINT_NAME = 'quancheck';
 
 -- Not Null
 ALTER TABLE Doctor MODIFY degree VARCHAR(50) NOT NULL;
@@ -170,5 +173,76 @@ SHOW INDEX FROM Doctor \G
 DROP INDEX idx_doctor_name ON Doctor;
 
 -- Relational operators
+-- 1.Greater than
+SELECT * FROM Pharmacy WHERE price > 80;
 
+-- 2.Less than
+SELECT * FROM Pharmacy WHERE price < 50;
+
+-- 3. Equal to
+SELECT * FROM Pharmacy WHERE quantity = 90;
+
+-- 4.Greater than equal to
+SELECT * FROM Pharmacy WHERE quantity >= 100;
+
+-- 5.Less than equal to
+SELECT * FROM Pharmacy WHERE quantity <= 40;
+SELECT * FROM Pharmacy WHERE quantity <= 80;
+
+-- 6.Not equal to
+SELECT * FROM Pharmacy WHERE price <> 500;
+
+-- Boolean operators
+-- AND
+SELECT * FROM Patient WHERE dob > '2003-01-01' AND address = "Mumbai, Maharashtra";
+
+-- OR
+SELECT * FROM Patient WHERE address = "Mumbai, Maharashtra" OR address = "Satara, Maharashtra";
+
+-- NOT
+SELECT * FROM Patient WHERE NOT email = 'sshinde@pqr.com';
+
+-- Pattern matching
+-- 1.Matches any string with %word% format
+-- word = Pediatrics
+SELECT * FROM Doctor WHERE specialization LIKE '%Pediatrics%';
+
+-- 2. Matches any string with %word format
+-- word = Physician
+SELECT * FROM Doctor WHERE specialization LIKE '%Physician';
+
+-- 3. Matches any string with word% format
+-- word = D
+SELECT * FROM Doctor WHERE specialization LIKE 'D%';
+
+-- 4.Matches exactly characters given in underscore with _____
+SELECT * FROM Doctor WHERE degree LIKE '______MS';
+
+-- Arithmetic operators
+-- Addition
+ SELECT med_name, price + 20 FROM Pharmacy;
+
+-- Subtraction
+SELECT med_name, price - 10 FROM Pharmacy;
+
+-- Multiplication
+SELECT med_name, quantity * 20 FROM Pharmacy;
+
+-- Division
+mysql> SELECT med_name, quantity / 10 FROM Pharmacy;
+
+-- Modulo
+SELECT code_no, med_name, quantity, 
+    CASE
+        WHEN quantity % 2 = 0 THEN 'Even'
+        ELSE 'Odd'
+    END AS quantity_type
+FROM Pharmacy;
+
+SELECT code_no, med_name, price,
+   CASE
+     WHEN price % 2 = 0 THEN 'Even'
+     ELSE 'Odd'
+   END AS price_type
+FROM Pharmacy;
 
